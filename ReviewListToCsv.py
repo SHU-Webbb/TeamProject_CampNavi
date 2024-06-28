cc33ab836656ef727c9177c06ab3a62a951b9d66
Siwoo Lee <leeshu.zip@gmail.com>
2024-06-28 19:52:58 +0900

    레포지토리 이동

---
+++ ReviewListToCsv.py
@@ -0,0 +1,26 @@
+import sys
+import pandas as pd
+import os
+
+file_name = 'Review.csv'
+new_data = pd.read_csv(sys.argv[1], encoding='utf-8')
+new_data.set_index('vseq', inplace=True)
+data = pd.DataFrame()
+
+try:
+    if os.path.exists(file_name):
+        original_data = pd.read_csv(file_name, encoding='utf-8')
+        original_data.set_index('vseq', inplace=True)
+        data = pd.concat([original_data, new_data])
+    else:
+        data = new_data
+except:
+    data = new_data
+    
+data.to_csv(file_name, sep=',', encoding='utf-8-sig')
+
+while True:
+    if os.path.exists(file_name):
+        break
+
+print('success')
